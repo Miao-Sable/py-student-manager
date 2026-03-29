0a97b1fe0b108563a266c742afc5116464fc2396
Sable <2633919784@qq.com>
2026-03-29 18:14:15 +0800

    修改了添加学生功能

--- main.py
+++ main.py
@@ -9,30 +9,31 @@ def show_menu():
     print("0.退出")
 
 def add_student(students):
-# 添加学生
     print("---添加学生---")
     stu_id = input("学号：")
-#检查学号是否重复
+
+    # 1. 先检查学号是否重复
     for s in students:
         if s['id'] == stu_id:
             print("学号已经存在，添加失败")
-            return
-        else:
-            name = input("姓名：")
-#输入成绩，并处理非法输入
+            return  # 重复则直接结束函数
+
+    # 2. 学号不重复，再输入姓名和成绩
+    name = input("姓名：")
     try:
-        math = float(input("数学成绩:"))
-        chinese = float(input("语文成绩"))
-        english = float(input("英语成绩"))
-    except  ValueError:
+        math = float(input("数学成绩："))
+        chinese = float(input("语文成绩："))
+        english = float(input("英语成绩："))
+    except ValueError:
         print("成绩必须是数字，添加失败")
         return
+
     student = {
         'id': stu_id,
         'name': name,
         'math': math,
-        'chinese':chinese,
-        'english':english
+        'chinese': chinese,
+        'english': english
     }
     students.append(student)
     print("添加成功")
