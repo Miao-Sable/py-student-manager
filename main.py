ea4c32e8f0616e561e57fc8a1739cd0429625123
Sable <2633919784@qq.com>
2026-03-29 17:36:33 +0800

    完成了“显示菜单”

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World")
+def showMenu():
+#---显示菜单---
+print("---学生成绩管理系统---")
+print("1.添加学生")
+print("2.查看所有学生")
+print("3.删除学生")
+print("4.修改学生")
+print("5.查找学生")
+print("0.退出")
+
+
